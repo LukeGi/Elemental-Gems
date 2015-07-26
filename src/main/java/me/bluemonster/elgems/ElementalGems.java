03ac4656ccf10513658065115229255c26d0d840
blue_monster122 <boo122333@gmail.com>
2015-07-26 23:26:41 +0100

    Completely fixes all the problems
    Signed-off-by: blue_monster122 <boo122333@gmail.com>

---
+++ src/main/java/me/bluemonster/elgems/ElementalGems.java
@@ -0,0 +1,37 @@
+package me.bluemonster.elgems;
+
+import cpw.mods.fml.common.Mod;
+import cpw.mods.fml.common.Mod.EventHandler;
+import cpw.mods.fml.common.event.FMLInitializationEvent;
+import cpw.mods.fml.common.event.FMLPostInitializationEvent;
+import cpw.mods.fml.common.event.FMLPreInitializationEvent;
+
+/**
+ * @author bluemonster122 <boo122333@gmail.com>
+ */
+
+@Mod(modid = ElementalGems.MODID, name = ElementalGems.MODNAME, version = ElementalGems.VERSION)
+public class ElementalGems
+{
+    public static final String MODID = "elgems";
+    public static final String MODNAME = "Elemental Gems";
+    public static final boolean ALPHA = true, BETA = false;
+    public static final String VERSION = (ALPHA ? "A-" : BETA ? "B-" : "RC") + "1.0";
+
+    @EventHandler
+    public void preInit(FMLPreInitializationEvent event)
+    {
+        
+    }
+
+    @EventHandler
+    public void Init(FMLInitializationEvent event)
+    {
+    }
+
+    @EventHandler
+    public void postInit(FMLPostInitializationEvent event)
+    {
+    }
+
+}
