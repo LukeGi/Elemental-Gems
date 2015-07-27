a4f9f971b1b588c5a520e2d1fc82f2f432b3b803
blue_monster122 <boo122333@gmail.com>
2015-07-27 16:15:55 +0100

    Adds texture for water_gem and still trying to figure the resources issue.
    Signed-off-by: blue_monster122 <boo122333@gmail.com>

--- src/main/java/me/bluemonster/elgems/blocks/ModBlock.java
+++ src/main/java/me/bluemonster/elgems/blocks/ModBlock.java
@@ -1,11 +1,14 @@
 package me.bluemonster.elgems.blocks;
 
 import cpw.mods.fml.common.registry.GameRegistry;
+import cpw.mods.fml.relauncher.Side;
+import cpw.mods.fml.relauncher.SideOnly;
 import me.bluemonster.elgems.creativetabs.EGCreativetabs;
 import me.bluemonster.elgems.reference.Reference;
 import me.bluemonster.elgems.registry.ModRegistry;
 import net.minecraft.block.Block;
 import net.minecraft.block.material.Material;
+import net.minecraft.client.renderer.texture.IIconRegister;
 
 /**
  * @author bluemonster122 <boo122333@gmail.com>
@@ -18,10 +21,28 @@ public class ModBlock extends Block
     {
         super(material);
         this.name = name;
-        setBlockName(Reference.MOD_ID + "_" + this.name);
-        setBlockTextureName(Reference.MOD_ID + ":" + this.name);
+        setBlockName(this.name);
+        setBlockTextureName(this.name);
         setCreativeTab(EGCreativetabs.GTAB);
         GameRegistry.registerBlock(this, this.name);
         ModRegistry.registeredBlocks.add(this);
+    }
+
+    @Override
+    public String getUnlocalizedName()
+    {
+        return String.format("tile.%s%s", Reference.MOD_ID.toLowerCase() + ":", getUnwrappedUnlocalizedName(super.getUnlocalizedName()));
+    }
+
+    @Override
+    @SideOnly(Side.CLIENT)
+    public void registerBlockIcons(IIconRegister iconRegister)
+    {
+        blockIcon = iconRegister.registerIcon(String.format("%s", getUnwrappedUnlocalizedName(this.getUnlocalizedName())));
+    }
+
+    protected String getUnwrappedUnlocalizedName(String unlocalizedName)
+    {
+        return unlocalizedName.substring(unlocalizedName.indexOf(".") + 1);
     }
 }
--- src/main/java/me/bluemonster/elgems/items/ModItem.java
+++ src/main/java/me/bluemonster/elgems/items/ModItem.java
@@ -1,10 +1,14 @@
 package me.bluemonster.elgems.items;
 
 import cpw.mods.fml.common.registry.GameRegistry;
+import cpw.mods.fml.relauncher.Side;
+import cpw.mods.fml.relauncher.SideOnly;
 import me.bluemonster.elgems.creativetabs.EGCreativetabs;
 import me.bluemonster.elgems.reference.Reference;
 import me.bluemonster.elgems.registry.ModRegistry;
+import net.minecraft.client.renderer.texture.IIconRegister;
 import net.minecraft.item.Item;
+import net.minecraft.item.ItemStack;
 
 /**
  * @author bluemonster122 <boo122333@gmail.com>
@@ -15,10 +19,34 @@ public class ModItem extends Item
 
     public ModItem(String name){
         this.name = name;
-        setUnlocalizedName(Reference.MOD_ID + "_" + this.name);
-        setTextureName(Reference.MOD_ID + ":" + this.name);
+        setUnlocalizedName(this.name);
+        setTextureName(this.name);
         setCreativeTab(EGCreativetabs.GTAB);
         GameRegistry.registerItem(this, this.name);
         ModRegistry.registeredItems.add(this);
+    }
+
+    @Override
+    public String getUnlocalizedName()
+    {
+        return String.format("item.%s%s", Reference.MOD_ID.toLowerCase() + ":", getUnwrappedUnlocalizedName(super.getUnlocalizedName()));
+    }
+
+    @Override
+    public String getUnlocalizedName(ItemStack itemStack)
+    {
+        return String.format("item.%s%s", Reference.MOD_ID.toLowerCase() + ":", getUnwrappedUnlocalizedName(super.getUnlocalizedName()));
+    }
+
+    @Override
+    @SideOnly(Side.CLIENT)
+    public void registerIcons(IIconRegister iconRegister)
+    {
+        itemIcon = iconRegister.registerIcon(this.getUnlocalizedName().substring(this.getUnlocalizedName().indexOf(".") + 1));
+    }
+
+    protected String getUnwrappedUnlocalizedName(String unlocalizedName)
+    {
+        return unlocalizedName.substring(unlocalizedName.indexOf(".") + 1);
     }
 }
